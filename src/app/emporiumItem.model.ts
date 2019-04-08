0fe5b0a081dd9af2103383aba7bd55179e364f96
Slaine-Skelton <bretthenning@hotmail.com>
2019-04-08 14:10:04 +0100

    final final

--- src/app/emporiumItem.model.ts
+++ src/app/emporiumItem.model.ts
@@ -1,6 +1,5 @@
 import { AvatarImageComponent } from './avatar-image/avatar-image.component';
 import { LibraryNameComponent } from './library-name/library-name.component';
-import { BookCountComponent } from './book-count/book-count.component';
 import { RatingComponent } from './rating/rating.component';
 
 /**
--- src/app/services/user-data.service.ts
+++ src/app/services/user-data.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { userInfo } from 'os';
 
 @Injectable({
   providedIn: 'root'
